24c20b34a5e2a2b8581f385030370154900b6655
Bradley Sutton <brad.kh.sutton@gmail.com>
2017-07-25 20:56:15 -0400

    Add private instance variables to Abstract Planet Class

--- Abstract_Planets/Planet.cs
+++ Abstract_Planets/Planet.cs
@@ -7,13 +7,20 @@ using System.Text;
 /// Name: Bradley Sutton
 /// Date: July 25, 2017
 /// Description: This is the Abstract Planet Class
-/// Version 0.1 - Creation of Abstract Planet Class and Template
+/// Version 0.2 - Add private instance variables
 /// </summary>
 namespace Abstract_Planets
 {
     public abstract class Planet
     {
         //PRIVATE INSTANCE VARIABLES (OR FIELDS)
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
 
         //PUBLIC PROPERTIES
 
