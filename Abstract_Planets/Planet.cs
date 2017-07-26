3c49adb068b527e632be8133801ddc4b993a2ad1
Bradley Sutton <brad.kh.sutton@gmail.com>
2017-07-25 20:55:18 -0400

    Add document to main Create Diagram Create abstract planet class and template

---
+++ Abstract_Planets/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Name: Bradley Sutton
+/// Date: July 25, 2017
+/// Description: This is the Abstract Planet Class
+/// Version 0.1 - Creation of Abstract Planet Class and Template
+/// </summary>
+namespace Abstract_Planets
+{
+    public abstract class Planet
+    {
+        //PRIVATE INSTANCE VARIABLES (OR FIELDS)
+
+        //PUBLIC PROPERTIES
+
+        //CONSTRUCTORS  ==========================================================
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
--- Abstract_Planets/Program.cs
+++ Abstract_Planets/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Name: Bradley Sutton
+/// Date: July 25, 2017
+/// Description: Assignment 4 - Abstract Planets
+/// Version 0.1 - Inital Documentation and Project Creation
+/// </summary>
+
 namespace Abstract_Planets
 {
     class Program
