e06719815474f3ac581992daf45b0a997b4f49f6
Bradley Sutton <brad.kh.sutton@gmail.com>
2017-07-26 00:21:04 -0400

    Add main constructor to the TerrestrialPlanet Class

--- Abstract_Planets/TerrestrialPlanet.cs
+++ Abstract_Planets/TerrestrialPlanet.cs
@@ -7,17 +7,22 @@ using System.Text;
 /// Name: Bradley Sutton
 /// Date: July 25, 2017
 /// Description: This is the Terrestrial Planet Class
-/// Version 0.1 - Extended for the implimentation of IHasMoon and IHabitable interfaces
+/// Version 0.2 - Add main constructor for the TerrestrialPlanet Class
 /// </summary>
 namespace Abstract_Planets
 {
     public class TerrestrialPlanet : Planet, IHasMoon, IHabitable
     {
         //PRIVATE INSTANCE VARIABLES
+        private bool _oxygen;
 
         //PRIVATE PROPERTIES
 
         //CONSTRUCTORS
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base(name, diameter, mass)
+        {
+            oxygen = this._oxygen;
+        }
 
         //PRIVATE METHODS
 
