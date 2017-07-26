699d9c17db1c3bf5bf1a73d2b0e93bfef272d800
Bradley Sutton <brad.kh.sutton@gmail.com>
2017-07-26 00:12:53 -0400

    Create the Terresterial Planet Class extended from the Abstract Planets Class

---
+++ Abstract_Planets/TerrestrialPlanet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Name: Bradley Sutton
+/// Date: July 25, 2017
+/// Description: This is the Terrestrial Planet Class
+/// Version 0.1 - Create the Terrestrial Planet Class extending from the Abstract Planet Class
+/// </summary>
+namespace Abstract_Planets
+{
+    public class TerrestrialPlanet : Planet
+    {
+        //PRIVATE INSTANCE VARIABLES
+
+        //PRIVATE PROPERTIES
+
+        //CONSTRUCTORS
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
