c64a922f23daa06f6328873a58586d3182415013
Bradley Sutton <brad.kh.sutton@gmail.com>
2017-07-25 21:11:22 -0400

    Add the GiantPlanet SubClass

---
+++ Abstract_Planets/GiantPlanet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Bradley Sutton
+ * Date: July 25, 2017
+ * Description: This is the Giant Planet Class that extends the Planet Abstract Class
+ * Version 0.1 - Create the GiantPlanet subclass extending from Planet
+ */
+
+namespace Abstract_Planets
+{
+    public class GiantPlanet : Planet
+    {
+        //PRIVATE INSTANCE VARIABLES (OR FIELDS)
+
+        //PRIVATE PROPERTIES
+
+        //CONSTRUCTORS ------------------------------------------------------------------------
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
